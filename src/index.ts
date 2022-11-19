bb54fbc36ea07c97bb3f8b77c6fd9f3d4c6f7eec
Stefan Plösser <stefan@nvon.com>
2022-11-19 21:30:50 +0100

    seems to work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log(`huhu`);
+import chokidar from "chokidar";
+import dayjs from "dayjs";
+import { cp, stat } from "fs/promises";
+import { join } from "path";
+
+// Whenever the contents of the source folder change,
+//  we copy a timestamped version of it in the target folder.
+
+const sourceFolder = process.env.SOURCE_FOLDER || "/doesnotexist";
+const targetFolder = process.env.TARGET_FOLDER || "/doesnotexist";
+
+// This makes sure the folders exist
+await Promise.all([stat(sourceFolder), stat(targetFolder)]);
+
+// One-liner for current directory
+chokidar.watch(sourceFolder).on("change", async (event, path) => {
+  const targetFolderName = dayjs().format("YYYY-MM-DD___HH:mm:ss");
+  const targetFolderFull = join(targetFolder, targetFolderName);
+
+  await cp(sourceFolder, targetFolderFull, { recursive: true });
+  console.log(`Copied files to ${targetFolderFull}`);
+});
+
+export {};
